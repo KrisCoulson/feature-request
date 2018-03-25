b7fb8f790d0994fa38429f13a642958e8a7eaccc
Kris Coulson <kriscoulson@gmail.com>
2018-03-25 00:52:12 -0700

    server and web folders, initialize web folder with create-react-app
    This commit split the server and web into seperate folders and
    initialized the web folder with create-react-app. Additionally add
    koa-router and setup and index route for testing purposes.

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router  = new Router();
+
+router.get('/', ctx => {
+  ctx.body = {
+    test: 'hello world'
+  }
+})
+
+app.use(router.routes());
+
+app.listen(3001);
